dcbeaaa8d0ecf0c8c3c78a91705bc5822d702721
tobiasmesa <tobiasmesa030303@gmail.com>
2022-09-05 20:28:55 -0300

    Subiendo proyecto

---
+++ Estanteria/Clases/Estante.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estanteria.Clases
+{
+    internal class Estante
+    {
+        private int _ubicacion;
+        private Producto[] _productos;
+
+        private Estante(int capacidad)
+        {
+            _productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion): this(capacidad)
+        {
+            _ubicacion = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return _productos;
+        }
+
+        public static string MostrarEstante(Estante estante)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Ubicacion:{estante._ubicacion}");
+            sb.AppendLine("Productos: ");
+            foreach (var prod in estante.GetProductos())
+            {
+                sb.AppendLine($"{Producto.MostrarProducto(prod)}");
+            }
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Estante estante, Producto producto)
+        {
+            bool rtn = false;
+
+            foreach (var prod in estante.GetProductos())
+            {
+                if(producto == prod)
+                {
+                    rtn = true;
+                }
+            }
+            return rtn;
+        }
+
+        public static bool operator !=(Estante estante, Producto producto)
+        {
+            return !(estante == producto);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool rtn = false;
+
+            for(int i = 0; i < e.GetProductos().Length; i++)
+            {
+                if (e._productos[i] is null)
+                {
+                    if (e == p)
+                    {
+                        e._productos[i] = p;
+                        rtn = true;
+                    }
+                }
+            }
+
+            return rtn;
+        }
+
+        public static bool operator -(Estante e, Producto p)
+        {
+            for (int i = 0; i < e.GetProductos().Length; i++)
+            {
+                if (e._productos[i] is not null)
+                {
+                    if (e == p)
+                    {
+                        e._productos[i] = null;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ Estanteria/Clases/Producto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estanteria.Clases
+{
+    internal class Producto
+    {
+        private string _codigoDeBarra = "";
+        private string _marca;
+        private float _precio;
+
+        public Producto(string marca, string codigoDeBarra, float precio)
+        {
+            _codigoDeBarra = codigoDeBarra;
+            _marca = marca;
+            _precio = precio;
+        }
+
+
+        public string GetMarca()
+        {
+            return _marca;
+        }
+        public float GetPrecio()
+        {
+            return _precio;
+        }
+
+        public static string MostrarProducto(Producto prod)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Marca:");
+            sb.AppendLine(prod._marca);
+            sb.Append("Precio:");
+            sb.AppendLine(prod._precio.ToString());
+            sb.Append("Codigo de barras:");
+            sb.AppendLine((string)prod);
+            return sb.ToString();
+
+        }
+
+
+        public static explicit operator string(Producto prod)
+        {
+            return prod._codigoDeBarra;
+        }
+        
+        public static bool operator == (Producto p1, Producto p2)
+        {
+            return (string)p1 == (string)p2;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public static bool operator ==(Producto p1, string str)
+        {
+            return p1.GetMarca() == str;
+        }
+        public static bool operator !=(Producto p1, string str)
+        {
+            return !(p1.GetMarca() == str);
+        }
+    }
+
+}
